e5af9185705c76ca30d7c659dfbe9ae50e06a4a0
parkash-arjan <parkash.arjan.github@gmail.com>
2018-01-01 16:56:49 -0500

    fixed code compilation error and formatted code.

--- Solidity/SoftKoin/SoftKoin.sol
+++ Solidity/SoftKoin/SoftKoin.sol
@@ -3,20 +3,11 @@ pragma solidity ^0.4.18;
 
 contract SoftKoin{
     address private owner;
-    string private  constant name = "SoftKoin";
-    string private constant symbol = "SFK";
+    string public  constant name = "SoftKoin";
+    string public constant symbol = "SFK";
     mapping(address=>uint) private tokenHolderBalances;
     mapping(address=>mapping(address=>uint)) private tokenHolderApprovals;
-    uint private totalSupply;
-
-    function name() public constant returns (string){
-        return name;
-    }
-
-    function symbol() public constant returns (string){
-        return symbol;
-    }
-
+    uint private totalTokenSupply;
     mapping(address=>uint) coinHolders;
 
     function SoftKoin() public{
@@ -39,7 +30,7 @@ contract SoftKoin{
 
     // ERC20
     function totalSupply() public constant returns (uint){
-        return totalSupply;
+        return totalTokenSupply;
     }
 
     function balanceOf(address tokenOwner) public constant returns (uint balance){
@@ -70,7 +61,8 @@ contract SoftKoin{
     }
 
     function transferFrom(address from, address to, uint tokens) public returns (bool success){
-        if(msg.sender==to && tokenHolderApprovals[from][to] >0 && tokenHolderApprovals[from]>0){
+        /*TODO: need to re-think about msg.sender==to condition*/
+        if(msg.sender==to && tokenHolderApprovals[from][to] >0 && tokenHolderBalances[from]>0){
             tokenHolderBalances[from] = tokenHolderBalances[from]  - tokens;
             tokenHolderBalances[to] = tokenHolderBalances[to]  + tokens;
             tokenHolderApprovals[from][to] = tokenHolderApprovals[from][to] - tokens;
