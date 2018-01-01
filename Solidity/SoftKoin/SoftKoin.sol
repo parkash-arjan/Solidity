58dd203905960e0609f140b4ec05e6ba3529c0c0
parkash-arjan <parkash.arjan.github@gmail.com>
2018-01-01 13:59:34 -0500

    Updated logic and constructor.

--- Solidity/SoftKoin/SoftKoin.sol
+++ Solidity/SoftKoin/SoftKoin.sol
@@ -1,9 +1,27 @@
 pragma solidity ^0.4.18;
 
 
-contract SoftKoin {
+contract SoftKoin{
     address owner;
-    function Softcoin (){
+    mapping(address=>uint) coinHolders;
+
+    function SoftKoin() public{
         owner = msg.sender;
     }
+
+    function giveCoins(uint numCoins  , address receiver) public{
+        //owner can never run out of coins , so he can send as many coins as he wants.
+        if(msg.sender ==owner && numCoins>0){
+            coinHolders[receiver] = coinHolders[receiver] + numCoins;
+        } else {
+            revert();
+        }
+    }
+
+    function viewBalance()public view returns (uint){
+        // return the balance of caller
+        return coinHolders[msg.sender];
+    }
 }
+
+
