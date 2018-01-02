2e2d6f69b194b3e8586fab8a520f4da206192f4e
parkash-arjan <parkash.arjan.github@gmail.com>
2018-01-01 19:01:25 -0500

    added erc20 Transfer(...) and Approval(...) events.

--- Solidity/SoftKoin/SoftKoin.sol
+++ Solidity/SoftKoin/SoftKoin.sol
@@ -7,11 +7,12 @@ contract SoftKoin{
     string public constant symbol = "SFK";
     mapping(address=>uint) private tokenHolderBalances;
     mapping(address=>mapping(address=>uint)) private tokenHolderApprovals;
-    uint private totalTokenSupply;
+    uint private totalTokenSupply = 1000000;
     mapping(address=>uint) coinHolders;
 
     function SoftKoin() public{
         owner = msg.sender;
+        tokenHolderBalances[msg.sender] = totalTokenSupply;
     }
 
     function giveCoins(uint numCoins  , address receiver) public{
@@ -29,6 +30,7 @@ contract SoftKoin{
     }
 
     // ERC20
+    // https://theethereum.wiki/w/index.php/ERC20_Token_Standard
     function totalSupply() public constant returns (uint){
         return totalTokenSupply;
     }
@@ -41,6 +43,7 @@ contract SoftKoin{
         if(tokenHolderBalances[msg.sender]>tokens && tokens>0){
             tokenHolderBalances[msg.sender] = tokenHolderBalances[msg.sender] - tokens;
             tokenHolderBalances[to] = tokenHolderBalances[to] + tokens;
+            Transfer(msg.sender,to,tokens);
             return true;
         } else {
             return false;
@@ -50,6 +53,7 @@ contract SoftKoin{
     function approve(address spender, uint tokens) public returns (bool success){
         if(tokenHolderBalances[msg.sender]>tokens){
             tokenHolderApprovals[msg.sender][spender] = tokens;
+            Approval(msg.sender,spender,tokens);
             return true;
         } else {
             return false;
@@ -61,15 +65,21 @@ contract SoftKoin{
     }
 
     function transferFrom(address from, address to, uint tokens) public returns (bool success){
-        /*TODO: need to re-think about msg.sender==to condition*/
-        if(msg.sender==to && tokenHolderApprovals[from][to] >0 && tokenHolderBalances[from]>0){
+        // if(tokens>0 && tokenHolderBalances[from]>0 && tokenHolderApprovals[from][msg.sender] >0 && tokenHolderApprovals[from][msg.sender] >tokens){
+        if(tokens>0 && tokenHolderBalances[from]>0  && tokenHolderApprovals[from][msg.sender] >tokens){
             tokenHolderBalances[from] = tokenHolderBalances[from]  - tokens;
             tokenHolderBalances[to] = tokenHolderBalances[to]  + tokens;
             tokenHolderApprovals[from][to] = tokenHolderApprovals[from][to] - tokens;
+            Transfer(from,to,tokens);
+            return true;
         } else {
             return false;
         }
     }
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
 
 
